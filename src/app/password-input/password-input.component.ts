1a9135835249fb9ccc45081c77af27a3cba1e85d
KsanaSal <oasalivon@gmail.com>
2023-02-23 16:42:21 +0200

    Optimisation in components

--- src/app/password-input/password-input.component.ts
+++ src/app/password-input/password-input.component.ts
@@ -1,19 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-password-input',
   templateUrl: './password-input.component.html',
-  styleUrls: ['./password-input.component.css']
+  styleUrls: ['./password-input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: PasswordInputComponent,
+      multi: true,
+    },
+  ],
 })
-export class PasswordInputComponent implements OnInit {
+export class PasswordInputComponent implements ControlValueAccessor {
+  onChange: any = () => {};
+  onTouched: any = () => {};
+  form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
-  form = this.formBuilder.group({
-    password:['', [Validators.required]]
-  })
+  isShowPassword = false;
+  force = 0;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      password: ['', [Validators.required]],
+    });
+    this.form.valueChanges.subscribe((value) => {
+      this.onChange(value.password);
+    });
   }
 
+  onChangeInput(event: Event): void {
+    const password = (event.target as HTMLInputElement).value;
+    this.form.setValue({ password });
+    this.onChange(password);
+    this.force = this.passwordStrength(this.form.controls['password'].value);
+  }
+  writeValue(value: string): void {
+    if (value) {
+      this.form.setValue({ password: value });
+    }
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  showPassword(inputField: HTMLInputElement) {
+    if (inputField.type === 'password') {
+      this.isShowPassword = true;
+      inputField.type = 'text';
+    } else {
+      this.isShowPassword = false;
+      inputField.type = 'password';
+    }
+  }
+
+  private passwordStrength(password: string) {
+    if (password.length < 8) {
+      return 0;
+    }
+    const letters = /[A-Za-z]/.test(password);
+    const numbers = /[0-9]/.test(password);
+    const symbols = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(password);
+    const array = [letters, numbers, symbols];
+
+    return array.filter((el) => el).length;
+  }
 }
--- src/app/password-input/password-strength-bar/password-strength-bar.component.ts
+++ src/app/password-input/password-strength-bar/password-strength-bar.component.ts
@@ -1,29 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-password-strength-bar',
   templateUrl: './password-strength-bar.component.html',
   styleUrls: ['./password-strength-bar.component.css'],
 })
-export class PasswordStrengthBarComponent implements OnChanges {
-  @Input() passwordToRead!: string | null | undefined;
+export class PasswordStrengthBarComponent {
+  @Input()
   force: number = 0;
-  constructor() {}
-
-  private passwordStrength(password: string) {
-    if (password.length < 8) {
-      return 0;
-    }
-    const letters = /[A-Za-z]/.test(password);
-    const numbers = /[0-9]/.test(password);
-    const symbols = /[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(password);
-    const array = [letters, numbers, symbols];
-
-    return array.filter((el) => el).length;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const password = changes['passwordToRead'].currentValue;
-    this.force = this.passwordStrength(password);
-  }
 }
